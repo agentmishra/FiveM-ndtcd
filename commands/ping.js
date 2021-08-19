c511f27a65f5abcee6d9607281568cc72eb7bfb4
Real Trickz <68506517+Realtrickz@users.noreply.github.com>
2021-08-19 13:11:06 +0300

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,4 @@
+module.exports = ({
+  name: "ping", // the command name here
+  code: `$title[Ping!] $description[Pong! it took **$pingms**!] $footer[Credits to Real trickz] $color[RANDOM]` // the code is located here!
+})
